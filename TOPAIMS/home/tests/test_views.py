6bd8bd8e811f8e884c667976679d7b8d84d38a8c
Alexander Beskine <a.d.beskine@outlook.com>
2017-11-10 18:40:05 +0000

    RED - login story

---
+++ TOPAIMS/home/tests/test_views.py
@@ -0,0 +1,28 @@
+from django.test import TestCase
+
+class HomePageTest(TestCase):
+
+	#-- HELPER METHODS --#
+
+	def login(self):
+		self.client.session['logged_in'] = True
+		self.client.session.save()
+
+	def logout(self):
+		self.client.session['logged_in'] = False
+		self.client.session.save()
+
+	#-- SETUP AND TEARDOWN --#
+
+	def setUp(self):
+		self.login()
+
+	def tearDown(self):
+		self.logout()
+
+	def test_homepage_redirects_logged_out_user(self):
+		self.logout()
+		response=self.client.get('/')
+
+		self.assertEquals(response, 401)
+		self.assertTemplateUsed(response, 'home/login.html')
