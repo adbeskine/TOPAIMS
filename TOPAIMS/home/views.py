6626a11eabf105374315f4f0bf16a8041ae82a83
Alexander Beskine <a.d.beskine@outlook.com>
2017-11-12 13:06:50 +0000

    REFRACTOR - refracted urls into home/urls.py, NOTE: will refractor a 'login_required'-esque decorator when it repeats 3 times

--- TOPAIMS/home/views.py
+++ TOPAIMS/home/views.py
@@ -3,16 +3,28 @@ from django.urls import reverse
 from django.http import HttpResponse
 from sensitive import WEBSITE_PASSWORD as password
 # Create your views here.
+# 
+# def login_check(function, *args, **kwargs):
+	# try:
+		# if request.session['logged_in'] == True:
+			# return request
+		# else:
+			# return redirect('/login/')
+	# except KeyError:
+		# return redirect('/login/')1
+
 
 def homepage(request):
-
 	try:
 		if request.session['logged_in'] == True:
 			pass
+		else:
+			return redirect(reverse('login'))
 	except KeyError:
 		return redirect(reverse('login'))
-
+	
 	return render(request, 'home/home.html')
+
 
 
 def login(request):
