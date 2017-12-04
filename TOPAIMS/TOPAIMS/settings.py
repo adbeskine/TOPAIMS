a9c71ce822dacebbbee53d5060c5149637ec1e09
Alexander Beskine <a.d.beskine@outlook.com>
2017-12-04 10:49:53 +0000

    preparing for deployment

--- TOPAIMS/TOPAIMS/settings.py
+++ TOPAIMS/TOPAIMS/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'wq-!19xb)wq0yr7%+ls0l%ojq3xij%=b1kf8g^haw3t83q&)k0'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = [*]
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
